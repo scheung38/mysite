7f0917b89e2a25eeac0c1587f8b9e17b1ed7121d
Sebastian Cheung <sebastian_cheung@yahoo.com>
2017-04-01 02:12:10 +0100

    added requirements.txt

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,9 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-        default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
     default = timezone.now
-    published = models.DateTimeField(
-        blank=True, null=True)
+    published = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
